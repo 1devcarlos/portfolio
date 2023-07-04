e26a19c5a9213edc7bf78220f2c657cddc9a96f2
Carlos Gomes <dev.shinsei@gmail.com>
2023-07-03 23:12:37 -0300

    fix: backtotop widget fixed

--- app/components/BackToTop/index.tsx
+++ app/components/BackToTop/index.tsx
@@ -10,18 +10,24 @@ export const BackToTop = () => {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   const handleScroll = useCallback(() => {
-    if (!show && window.scrollY > 500) setShow(true);
-    if (show && window.scrollY <= 500) setShow(false);
+    if (!show && window.scrollY > 500) {
+      setShow(true);
+    } else if (show && window.scrollY <= 500) {
+      setShow(false);
+    }
   }, [show]);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
 
   return (
-    <div className="fixed right-4 bottom-4 z-20">
+    <div className={`fixed right-4 bottom-4 z-20 ${show ? "block" : "hidden"}`}>
       <Button onClick={scrollToTop} className="shadow-lg shadow-rose/20">
         <TbArrowNarrowUp size={20} />
       </Button>

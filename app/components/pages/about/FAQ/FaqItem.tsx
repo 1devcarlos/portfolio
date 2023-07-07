61d781ab94b9509cdf9fca45b24264359b2dbf2b
Carlos Gomes <dev.shinsei@gmail.com>
2023-07-07 17:47:01 -0300

    style: fix responsivity on about page

--- app/components/pages/about/FAQ/FaqItem.tsx
+++ app/components/pages/about/FAQ/FaqItem.tsx
@@ -19,18 +19,18 @@ export const FaqItem = ({ question, answer }: FaqItemProps) => {
 
   return (
     <div
-      className="w-[830px] my-2 sm:my-4 md:my-6 shadow-sm cursor-pointer border-[1px] border-rose rounded-lg transition-all duration-200 ease-in"
+      className="w-[350px] sm:w-[630px] my-2 sm:my-4 md:my-6 shadow-sm cursor-pointer border-[1px] border-rose rounded-lg transition-all ease"
       onClick={handleToggle}
     >
       <div className="px-6 text-center items-center h-20 select-none flex justify-between flex-row">
         <h5 className="text-gray-400">{question}</h5>
-        <div className="flex-none pl-2 transition-all duration-200 ease-in">
+        <div className="flex-none pl-2 transition-all ease">
           {expanded ? minusIcon : plusIcon}
         </div>
       </div>
       <div
-        className={`px-6 pt-0 overflow-hidden transition-all duration-200 ease-in ${
-          expanded ? "max-h-40" : "max-h-0"
+        className={`px-6 pt-0 overflow-hidden transition-all ease ${
+          expanded ? "max-h-64" : "max-h-0"
         }`}
       >
         <p className="pb-4 text-center text-gray-400">{answer}</p>

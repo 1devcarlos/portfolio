e1a2712d4d0eed5b5999136cd88488d966b52be5
Carlos Gomes <dev.shinsei@gmail.com>
2023-07-07 23:51:23 -0300

    fix: contact page metadata fixed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,4 @@
-"use client";
-
-import { motion } from "framer-motion";
-import { HiArrowNarrowLeft } from "react-icons/hi";
-import { Link } from "../components/Link";
-import { SectionTitle } from "../components/pages/home/SectionTitle";
+import { PageIntroduction } from "./PageIntroduction";
 
 export const metadata = {
   title: "Contato",
@@ -11,30 +6,8 @@ export const metadata = {
 
 export default function Contact() {
   return (
-    <section className="w-full h-[350px] lg:h-[630px] flex flex-col items-center justify-center px-2">
-      <SectionTitle
-        subtitle="hire"
-        title="Contratar"
-        className="text-center items-center [&>h3]:text-4xl"
-      />
-
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 100 }}
-        transition={{ duration: 0.6 }}
-        className="flex flex-col items-center"
-      >
-        <p className="text-gray-400 text-center max-w-[640px] my-6 text-sm sm:text-base">
-          Quer transformar sua próxima idea de aplicação em realidade ? Entre em
-          contato para discutirmos seus próximos passos!.
-        </p>
-
-        <Link href="/">
-          <HiArrowNarrowLeft size={20} />
-          Voltar a Página Inicial
-        </Link>
-      </motion.div>
-    </section>
+    <>
+      <PageIntroduction />
+    </>
   );
 }

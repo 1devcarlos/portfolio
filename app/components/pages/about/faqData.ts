cb8b26f0be79ca89ffc4401d60744a66ed617170
Carlos Gomes <dev.shinsei@gmail.com>
2023-09-28 00:57:22 -0300

    refactor: changes on the faqData

--- app/components/pages/about/faqData.ts
+++ app/components/pages/about/faqData.ts
@@ -1,52 +1,47 @@
 export const faqData = [
-  {
-    question: "Quais são as principais habilidades que possuo ?",
-    answer:
-      "Como desenvolvedor Frontend, possuo sólidos conhecimentos nas tecnologias da stack React, incluindo Next, Tailwindcss e Typescript. Além disso, tenho experiência em desenvolvimento Web FullStack e estou atualmente cursando minha graduação em Jogos Digitais, explorando o desenvolvimento de jogos.",
-  },
-  {
-    question: "Quais cursos e formações você possui?",
-    answer:
-      "Tenho formação em Análise e Desenvolvimento de Sistemas, o que me proporcionou uma base sólida em engenharia de software. Além disso, concluí o curso Explorer da Rocketseat, que foca no desenvolvimento web FullStack.",
-  },
-  {
-    question: "Quais são seus interesses além da área de desenvolvimento?",
-    answer:
-      "Além do desenvolvimento, sou um ávido jogador de FPS, amante de música e entusiasta de animes. Tenho um grande interesse por animes e assisti a uma ampla variedade deles. Ficaria muito feliz em conversar sobre esses assuntos.",
-  },
-  {
-    question: "Quais tipos de projetos você desenvolveu?",
-    answer:
-      "Tenho experiência no desenvolvimento de aplicações web dinâmicas e interativas usando React. Também estou explorando o desenvolvimento mobile e de jogos, ampliando meu conjunto de habilidades e minha criatividade.",
-  },
-  {
-    question: "Em quais tipos de oportunidades estou interessado?",
-    answer:
-      "Estou interessado em colaborar com empresas e pessoas que valorizam a inovação e a excelência. Busco oportunidades que me permitam aplicar meus conhecimentos em desenvolvimento frontend, mobile e de jogos, contribuindo para projetos desafiadores e impactantes.",
-  },
-  {
-    question: "Como entrar em contato comigo?",
-    answer:
-      "Você pode entrar em contato comigo através dos formulários de contato nesse site, onde você encontrará minhas informações de contato. Além disso também estou disponível no LinkedIn e WhatsApp, que também estão disponíveis neste site. Estou sempre aberto a novas oportunidades e adoraria conversar sobre possíveis colaborações.",
-  },
-  {
-    question: "Onde posso encontrar exemplos dos seus projetos anteriores?",
-    answer:
-      "Na aba de Projetos, você encontrará uma seleção dos meus projetos anteriores. Lá, você poderá ver o trabalho que realizei e as habilidades que foram utilizadas ao longo do desenvolvimento do projeto, sendo possível visitar o repositório como também vê-lo online.",
-  },
-  {
-    question: "Estou disponível para trabalhos freelancer?",
-    answer:
-      "Sim, estou disponível para trabalhos freelance. Se você tiver um projeto específico em mente, ficarei feliz em discutir os detalhes e ver como posso te ajudar.",
-  },
-  {
-    question: "Como abordo problemas de desenvolvimento?",
-    answer:
-      "Tenho uma abordagem analítica, onde procuro entender profundamente os problemas que surgem e desenvolver soluções eficazes. Minha atenção aos detalhes me permite entregar código de alta qualidade e buscar a excelência em todas as etapas do processo de desenvolvimento.",
-  },
-  {
-    question: "Você está aberto a oportunidades de aprendizado e crescimento?",
-    answer:
-      "Sim, estou sempre buscando aprender coisas novas e aprimorar minhas habilidades existentes. Estou aberto a oportunidades que me permitam expandir meu conhecimento e acompanhar as últimas tendências tecnológicas.",
-  },
+	{
+		question: "Quais as principais habilidades que possuo ?",
+		answer:
+			"Como desenvolvedor Frontend, possuo sólidos conhecimentos nas tecnologias da stack React, incluindo Next, Tailwindcss e Typescript. Além disso, tenho experiência em desenvolvimento Web FullStack e estou atualmente cursando minha graduação em Jogos Digitais, explorando o desenvolvimento de jogos."
+	},
+	{
+		question: "Quais cursos e formações eu tenho?",
+		answer:
+			"Tenho formação em Análise e Desenvolvimento de Sistemas, o que me proporcionou uma base sólida em engenharia de software. Além disso, concluí o curso Explorer da Rocketseat, que foca no desenvolvimento web FullStack."
+	},
+	{
+		question: "Quais tipos de projetos já desenvolvi?",
+		answer:
+			"Tenho experiência no desenvolvimento de aplicações web dinâmicas e interativas usando React. Também estou explorando o desenvolvimento mobile e de jogos, ampliando meu conjunto de habilidades e minha criatividade."
+	},
+	{
+		question: "Em quais tipos de oportunidades estou interessado?",
+		answer:
+			"Estou interessado em colaborar com empresas e pessoas que valorizam a inovação e a excelência. Busco oportunidades que me permitam aplicar meus conhecimentos em desenvolvimento frontend, mobile e de jogos, contribuindo para projetos desafiadores e impactantes."
+	},
+	{
+		question: "Como entrar em contato comigo?",
+		answer:
+			"Você pode entrar em contato comigo através dos formulários de contato nesse site, onde você encontrará minhas informações de contato. Além disso também estou disponível no LinkedIn e WhatsApp, que também estão disponíveis neste site. Estou sempre aberto a novas oportunidades e adoraria conversar sobre possíveis colaborações."
+	},
+	{
+		question: "Onde encontrar exemplos de meus projetos anteriores?",
+		answer:
+			"Na aba de Projetos, você encontrará uma seleção dos meus projetos anteriores. Lá, você poderá ver o trabalho que realizei e as habilidades que foram utilizadas ao longo do desenvolvimento do projeto, sendo possível visitar o repositório como também vê-lo online. Além disso você pode sempre acompanhar meu github e ver novidades por lá!"
+	},
+	{
+		question: "Estou disponível para trabalhos freelancer?",
+		answer:
+			"Sim, estou disponível para trabalhos freelance. Se você tiver um projeto específico em mente, ficarei feliz em discutir os detalhes e ver como posso te ajudar."
+	},
+	{
+		question: "Estou aberto a oportunidades de aprendizado e crescimento?",
+		answer:
+			"Sim, estou sempre buscando aprender coisas novas e aprimorar minhas habilidades existentes. Estou aberto a oportunidades que me permitam expandir meu conhecimento e acompanhar as últimas tendências tecnológicas."
+	},
+	{
+		question: "Quais são meus interesses além da área de desenvolvimento?",
+		answer:
+			"Além do desenvolvimento, sou um ávido jogador de FPS, amante de música e entusiasta de animes. Tenho um grande interesse por animes e assisti a uma ampla variedade deles. Ficaria muito feliz em conversar sobre esses assuntos."
+	}
 ];
